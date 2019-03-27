fe852c9df49b468cbfb1762dacb105424e800124
Mikael Karlgren <mikaelkarlgren@hotmail.se>
2019-03-27 09:47:05 +0100

    Changed configuration settings: Local certificate support as an alternative to azure key vault, toggled on test environment with additional claims, removed GrandId

--- samples/Standalone.MvcSample/Startup.cs
+++ samples/Standalone.MvcSample/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Azure.KeyVault;
+using System;
 
 namespace Standalone.MvcSample
 {
@@ -34,46 +35,32 @@ namespace Standalone.MvcSample
                     builder.AddSameDevice(BankIdAuthenticationDefaults.SameDeviceAuthenticationScheme, "BankID (SameDevice)", options => { })
                            .AddOtherDevice(BankIdAuthenticationDefaults.OtherDeviceAuthenticationScheme, "BankID (OtherDevice)", options => { });
 
+                    builder.Configure(options =>
+                    {
+                        options.IssueBirthdateClaim = true;
+                        options.IssueGenderClaim = true;
+                    });
+
                     if (Configuration.GetValue("ActiveLogin:BankId:UseSimulatedEnvironment", false))
                     {
                         builder.UseSimulatedEnvironment();
                     }
                     else if (Configuration.GetValue("ActiveLogin:BankId:UseTestEnvironment", false))
                     {
-                        builder.UseTestEnvironment()
-                            .UseRootCaCertificate(Path.Combine(_environment.ContentRootPath, Configuration.GetValue<string>("ActiveLogin:BankId:CaCertificate:FilePath")))
-                            .UseClientCertificateFromAzureKeyVault(Configuration.GetSection("ActiveLogin:BankId:ClientCertificate"));
+                        builder.UseTestEnvironment();
                     }
                     else
                     {
-                        builder.UseProductionEnvironment()
-                            .UseRootCaCertificate(Path.Combine(_environment.ContentRootPath, Configuration.GetValue<string>("ActiveLogin:BankId:CaCertificate:FilePath")))
-                            .UseClientCertificateFromAzureKeyVault(Configuration.GetSection("ActiveLogin:BankId:ClientCertificate"));
-                    }
-                })
-                .AddGrandId(builder =>
-                {
-                    builder.AddBankIdSameDevice(GrandIdAuthenticationDefaults.BankIdSameDeviceAuthenticationScheme, "GrandID (SameDevice)", options => { })
-                           .AddBankIdOtherDevice(GrandIdAuthenticationDefaults.BankIdOtherDeviceAuthenticationScheme, "GrandID (OtherDevice)", options => { })
-                           .AddBankIdChooseDevice(GrandIdAuthenticationDefaults.BankIdChooseDeviceAuthenticationScheme, "GrandID (ChooseDevice)", options => { });
-
-                    if (Configuration.GetValue("ActiveLogin:GrandId:UseSimulatedEnvironment", false))
-                    {
-                        builder.UseSimulatedEnvironment();
-                    }
-                    else if (Configuration.GetValue("ActiveLogin:GrandId:UseTestEnvironment", false))
-                    {
-                        builder.UseTestEnvironment(ConfigureEnvironment);
-                    }
-                    else
-                    {
-                        builder.UseProductionEnvironment(ConfigureEnvironment);
+                        builder.UseProductionEnvironment();
                     }
 
-                    void ConfigureEnvironment(IGrandIdEnvironmentConfiguration config)
+                    if (!Configuration.GetValue("ActiveLogin:BankId:UseSimulatedEnvironment", false))
                     {
-                        config.ApiKey = Configuration.GetValue<string>("ActiveLogin:GrandId:ApiKey");
-                        config.BankIdServiceKey = Configuration.GetValue<string>("ActiveLogin:GrandId:BankIdServiceKey");
+                        builder.UseRootCaCertificate(Path.Combine(_environment.ContentRootPath, Configuration.GetValue<string>("ActiveLogin:BankId:CaCertificate:FilePath")));
+                        if (Configuration.GetValue("ActiveLogin:BankId:ClientCertificate:UseAzureKeyVault", false))
+                            builder.UseClientCertificateFromAzureKeyVault(Configuration.GetSection("ActiveLogin:BankId:ClientCertificate:AzureKeyVault"));
+                        else
+                            builder.UseClientCertificate(() => GetCertificateFromStore(Configuration.GetValue<string>("ActiveLogin:BankId:ClientCertificate:Local:FileName")));
                     }
                 });
 
@@ -96,6 +83,36 @@ namespace Standalone.MvcSample
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        // obtained & slightly modified from https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509certificate2?view=netframework-4.7.2
+        private static X509Certificate2 GetCertificateFromStore(string certName)
+        {
+            // Get the certificate store for the current user.
+            X509Store store = new X509Store(StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+
+                // Place all certificates in an X509Certificate2Collection object.
+                X509Certificate2Collection certCollection = store.Certificates;
+                // If using a certificate with a trusted root you do not need to FindByTimeValid, instead:
+                // currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certName, true);
+                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+                X509Certificate2Collection signingCert = currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certName, false);
+                if (signingCert.Count == 0)
+                {
+                    signingCert = currentCerts.Find(X509FindType.FindBySubjectName, certName, false);
+                    if (signingCert.Count == 0)
+                        return null;
+                }
+                // Return the first certificate in the collection, has the right name and is current.
+                return signingCert[0];
+            }
+            finally
+            {
+                store.Close();
+            }
         }
     }
 }
